a4bff65e378489ef54d32acedf61e181baaa957e
MirMara <92723885+MirMara@users.noreply.github.com>
2021-11-15 19:00:31 +0100

    Add files via upload

---
+++ es.15.11/JVS/list.js
@@ -0,0 +1,19 @@
+import { render } from "./utils.js";
+
+const List = (data) => {
+  const elements = data
+    .map((item) => 
+    `<div><li><input 
+    type="checkbox" ${item.completed ? "checked" : ""}  /> ${item.title} ${item.expired} </li></div>`)
+    .join("");
+
+  const container = document.querySelector("#container");
+  render(
+    container,
+    `
+    <p> To Do </p>
+    <ul>${elements}</ul>`
+  );
+};
+
+export { List };
---
+++ es.15.11/JVS/utils.js
@@ -0,0 +1,7 @@
+
+ const API = "https://edgemony-backend.herokuapp.com/todos";
+
+ 
+ const render = (container, content) => (container.innerHTML = content);
+ 
+ export { API, render };
